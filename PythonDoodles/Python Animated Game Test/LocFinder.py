820318b6aa09a96591761df9bc08d488891bebd0
alexHattori <ahattori@mit.edu>
2015-05-26 08:09:40 -0700

    Editing Test

--- PythonDoodles/Python Animated Game Test/LocFinder.py
+++ PythonDoodles/Python Animated Game Test/LocFinder.py
@@ -5,7 +5,7 @@ screen = pygame.display.set_mode((width, height))
 running = True
 pygame.init()
 
-img = pygame.image.load('Shrek.png')
+img = pygame.image.load('sonicSprites.png')
 screen.blit(img,(0,0))
 while running:
     screen.blit(img,(0,0))
